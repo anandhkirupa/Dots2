482fe79b499a20f39b9051cf8fd566e7b75be5b8
kirupanandan-j <kirupanandan.j@thermofisher.com>
2024-03-15 14:13:35 +0530

    Dots2
    Entities instantiation in main thread

---
+++ Assets/Scripts/SpawnSystem.cs
@@ -0,0 +1,33 @@
+using Unity.Burst;
+using Unity.Collections;
+using Unity.Entities;
+using Unity.Mathematics;
+using Unity.Transforms;
+
+public partial struct SpawnSystem : ISystem
+{
+    [BurstCompile]
+    public void OnCreate(ref SystemState state)
+    {
+        state.RequireForUpdate<Spawner>();
+    }
+
+    [BurstCompile]
+    public void OnUpdate(ref SystemState state)
+    {
+        state.Enabled = false;
+
+        var prefab = SystemAPI.GetSingleton<Spawner>().CubePrefab;
+        var instances = state.EntityManager.Instantiate(prefab, 10, Allocator.Temp);
+
+        // randomly set the positions of the new cubes
+        // (we'll use a fixed seed, 123, but if you want different randomness 
+        // for each run, you can instead use the elapsed time value as the seed)
+        var random = new Random(123);
+        foreach (var entity in instances)
+        {
+            var transform = SystemAPI.GetComponentRW<LocalTransform>(entity);
+            transform.ValueRW.Position = random.NextFloat3(new float3(10, 10, 10));
+        }
+    }
+}
